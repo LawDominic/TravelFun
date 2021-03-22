5be27b69f4f01b3ab8981f80a3f198539f1ace80
Dominic Law <dominic.law79@gmail.com>
2021-03-22 20:22:01 +1100

    Removed database code

--- App.js
+++ App.js
@@ -3,25 +3,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AuthNavigator from './app/navigation/AuthNavigator'
 
-// import * as SQLite from 'expo-sqlite';
-
-// const db = SQLite.openDatabase('db.db')
-
 export default function App() {
-
-  // db.transaction(tx => {
-  //   tx.executeSql(
-  //     'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, fullName TEXT NOT NULL, email TEXT NOT NULL UNIQUE, password text NOT NULL)'
-  //   );
-  // });
-
-  // db.transaction(
-  //   tx => {
-  //     tx.executeSql(
-  //       'INSERT INTO users (fullName, email, password) values (?, ?)', ['admin', 'admin@admin.com', 'admin']
-  //     )
-  //   }
-  // )
 
   return (  
     <NavigationContainer>
