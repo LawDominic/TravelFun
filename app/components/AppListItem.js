d4ed3dfc3247c74d71b7f119d63b6b4fbf53624c
Dominic Law <dominic.law79@gmail.com>
2021-03-23 01:15:27 +1100

    Fixed ListItem

--- app/components/AppListItem.js
+++ app/components/AppListItem.js
@@ -3,7 +3,7 @@ import { Image, StyleSheet, View } from 'react-native';
 import AppColor from '../config/AppColor';
 import AppText from './AppText';
 
-function AppListItem({title, IconComponent}) {
+function AppListItem({title, titleStyle, IconComponent}) {
     return (
         <View style={styles.container}>
 
@@ -12,7 +12,7 @@ function AppListItem({title, IconComponent}) {
             </View>
 
             <View style={styles.textContainer}>
-                <AppText style={styles.title}>{title}</AppText>
+                <AppText style={styles[titleStyle]}>{title}</AppText>
             </View>
         </View>
     );
@@ -21,9 +21,12 @@ function AppListItem({title, IconComponent}) {
 const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
+        marginTop: 10,
     },
     icon: {
-        margin: 25,
+        marginLeft: 20,
+        marginRight: 20,
+        marginTop: 0,
     },
     textContainer: {
         flexDirection: 'column',
@@ -32,8 +35,14 @@ const styles = StyleSheet.create({
         color: AppColor.blackblue,
         fontSize: 18,
         fontWeight: 'bold',
-        marginTop: 25,
+        marginTop: 3,
     },
+    subtitle: {
+        color: AppColor.grey,
+        fontSize: 14,
+        fontWeight:'bold',
+        marginLeft: 2,
+    }
 })
 
 export default AppListItem;
