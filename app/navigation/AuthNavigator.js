7d7f18a9bde44cef5873b6697a6cd7f4052d19e4
Dominic Law <dominic.law79@gmail.com>
2021-03-23 01:18:08 +1100

    Auth code

--- app/navigation/AuthNavigator.js
+++ app/navigation/AuthNavigator.js
@@ -7,12 +7,16 @@ import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
 import HomeScreen from '../screens/HomeScreen';
 import SettingsScreen from '../screens/SettingsScreen';
+import ThingsToDoScreen from '../screens/ThingsToDoScreen';
 
 
 const AppStack = createStackNavigator();
 
 const AuthNavigator = () => (
     <AppStack.Navigator>
+        <AppStack.Screen name="Home" component={HomeScreen} options={{headerShown:false}}/>
+        <AppStack.Screen name="ThingsToDo" component={ThingsToDoScreen} options={{headerShown:false}}/>
+        <AppStack.Screen name="Settings" component={SettingsScreen} options={{headerShown:false}}/>
         <AppStack.Screen name="Welcome" component={WelcomeScreen} options={{headerShown:false}}/>
         <AppStack.Screen name="Login" component={LoginScreen} options={{headerShown:false}}/>
         <AppStack.Screen name="Register" component={RegisterScreen} options={{headerShown:false}}/>
