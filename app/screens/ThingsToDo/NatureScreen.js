79782c3046da4f5bdb17f0abeef40d1893940263
Dominic Law <dominic.law79@gmail.com>
2021-03-24 18:20:37 +1100

    Added nature & parks screen

---
+++ app/screens/ThingsToDo/NatureScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+
+import AppScreen from '../../components/AppScreen';
+import AppText from '../../components/AppText';
+
+
+function NatureScreen({navigation}) {
+    return (
+        <AppScreen style={styles.container}>
+            <View>
+                <AppText>asd</AppText>
+            </View>
+        </AppScreen>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }, 
+})
+
+export default NatureScreen;
