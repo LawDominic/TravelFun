1f29018e152a1b7afddf894f43e26df2eada87ee
Dominic Law <dominic.law79@gmail.com>
2021-03-24 18:16:15 +1100

    Updated screens

--- app/navigation/AuthNavigator.js
+++ app/navigation/AuthNavigator.js
@@ -8,18 +8,22 @@ import RegisterScreen from '../screens/RegisterScreen';
 import HomeScreen from '../screens/HomeScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 import ThingsToDoScreen from '../screens/ThingsToDoScreen';
+import NatureScreen from '../screens/ThingsToDo/NatureScreen';
+import MyFavoriteScreen from '../screens/MyFavoritesScreen';
 
 
 const AppStack = createStackNavigator();
 
 const AuthNavigator = () => (
-    <AppStack.Navigator>
-        <AppStack.Screen name="Home" component={HomeScreen} options={{headerShown:false}}/>
+    <AppStack.Navigator initialRouteName="ThingsToDo">
+        {/* <AppStack.Screen name="Home" component={HomeScreen} options={{headerShown:false}}/> */}
+        <AppStack.Screen name="MyFavorite" component={MyFavoriteScreen} options={{headerShown:false}}/>
         <AppStack.Screen name="ThingsToDo" component={ThingsToDoScreen} options={{headerShown:false}}/>
         <AppStack.Screen name="Settings" component={SettingsScreen} options={{headerShown:false}}/>
         <AppStack.Screen name="Welcome" component={WelcomeScreen} options={{headerShown:false}}/>
         <AppStack.Screen name="Login" component={LoginScreen} options={{headerShown:false}}/>
         <AppStack.Screen name="Register" component={RegisterScreen} options={{headerShown:false}}/>
+        <AppStack.Screen name="Nature" component={NatureScreen} options={{headerShown:false}}/>
     </AppStack.Navigator>
 )
 
