d1527e3fe51ac73819c5c2a1d668ddb3c662ba5e
Dominic Law <dominic.law79@gmail.com>
2021-03-25 22:40:31 +1100

    Changed color

--- app/config/AppColor.js
+++ app/config/AppColor.js
@@ -3,6 +3,7 @@ export default {
     black: '#000',
     blackblue: '#213B54',
     blue: '#66B2FF',
+    darkgrey: '#e1e1e1',
     green: '#6ab04c',
     grey: '#E7e8e9',
     lightgrey: '#f9f9f9',
