85ac8fae0d764d54008cbc52799e341925eaf815
Dominic Law <dominic.law79@gmail.com>
2021-03-25 22:39:10 +1100

    Cleaned up code

--- app/components/AppMinimalCard.js
+++ app/components/AppMinimalCard.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { Shadow } from 'react-native-shadow-2';
 
 import AppColor from '../config/AppColor';
 import AppText from './AppText';
 
-function AppMinimalCard({title, IconComponent}) {
+function AppMinimalCard({title, IconComponent, onPress}) {
     return (
-        <View style={styles.container}>
-            <View style={styles.icon}>
-                {IconComponent}
-            </View>
-
-            <View style={styles.textContainer}>
-                <AppText style={styles.title}>{title}</AppText>
-            </View>
+        <View style={styles.asd}>
+            <Shadow distance={10} radius={25} startColor={AppColor.darkgrey} offset={[0,2]}>
+                <TouchableOpacity onPress={onPress} style={styles.box}>
+                    <View style={styles.container}>
+                        <View style={styles.icon}>
+                            {IconComponent}
+                        </View>
+                        <View style={styles.textContainer}>
+                            <AppText style={styles.title}>{title}</AppText>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+            </Shadow>
         </View>
     );
 }
@@ -26,8 +32,8 @@ const styles = StyleSheet.create({
         borderRadius: 25,
         justifyContent: 'center',
     },
-    textContainer: {
-
+    asd: {
+        margin: 10,
     },
     title: {
         marginTop: 5,
